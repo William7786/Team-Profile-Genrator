e3dd1c70389bae5ed2d68c18eaeabcd0c410e3d8
Tcomp <williamt7786@gmail.com>
2021-06-07 16:24:33 -0400

    for general index.js const

---
+++ index.js
@@ -0,0 +1,23 @@
+const fs = require ('fs')
+const inquire = require ('inquirer')
+const Manager = require ()
+const Intern = require ()
+const engineer = require()
+
+
+
+
+
+//Questions 
+function  wtf(filename, data) {
+return fs.writeFileSync(path.join(filename), data)}
+
+const engineerQ = [
+{
+    type: "input",
+    name: "name",
+    message: ""
+}
+]
+const managerQ = []
+const internQ = []
