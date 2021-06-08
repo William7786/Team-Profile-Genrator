07c3e9a304cf6b051f157819925bd3def6a7b031
Tcomp <williamt7786@gmail.com>
2021-06-08 17:24:12 -0400

    This commit is for more general organization

--- index.js
+++ index.js
@@ -1,23 +1,75 @@
 const fs = require ('fs')
-const inquire = require ('inquirer')
-const Manager = require ()
-const Intern = require ()
-const engineer = require()
+const inquirer = require('inquirer')
+const path = require('path')
+// const Manager = require ()
+// const Intern = require ()
+// const Engineer = require('./Libr.EngineerHtml')
 
 
 
 
 
-//Questions 
-function  wtf(filename, data) {
-return fs.writeFileSync(path.join(filename), data)}
+//Write to file
+function wtf (fileName, data){
+    fs.writeFile(fileName, data , function(error) {
+      console.log(data)
+      
+      if(error){
+          return console.log(error)
+      } else {
+          console.log ("No errors here")
+      }
+        
+    })};
 
+//Engineer Questions
 const engineerQ = [
 {
     type: "input",
     name: "name",
-    message: ""
+    message: "What is the team engineers name",
 }
+
 ]
-const managerQ = []
-const internQ = []
+//Manager Questions
+const managerQ = [{}]
+
+
+//Intern Questions
+const internQ = [{}]
+
+//Builds engineer questions
+function buildE (){
+    inquirer.prompt(engineerQ)
+    .then(engineerA => {
+        const engineer = new Engineer()
+    })
+}
+
+// Builds Manager questions
+function buildM(){
+    inquirer.prompt(managerQ)
+    .then(managerA => {
+        const manager= new Manager()
+    })
+}
+// Builds Intern Questions
+function buildI (){
+    inquirer.prompt(internQ)
+    .then(InternA => {
+        const intern = new Intern()
+    })
+}
+
+
+function init() {
+    buildE()
+    buildM()
+    buildI()
+     .then(function(data){
+         wtf("New.html", generateMarkdown(data));
+         console.log(data)
+     })}
+
+
+init()
